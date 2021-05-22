a1ca360951a6965e09c14240835c7dd7d34efaff
Alexey Mokharev <lmokhareva@gmail.com>
2021-05-22 22:00:29 +0300

    ADDED: test functionality

---
+++ Choose_type.py
@@ -0,0 +1,117 @@
+import math
+
+
+def make_choice():
+    while True:
+
+        print("Выберите тип термоапары:")
+
+        print("1.   Платиновые ТС и ЧЭ, α = 0,00385 °С-1 ")
+
+        print("2.	 Платиновые ТС и ЧЭ, α = 0,00391 °С-1")
+
+        print("3.   Медные ТС и ЧЭ, α = 0,00428 °С-1")
+
+        print("4.   Никелевые ТС и ЧЭ, α = 0,00617 °С-1")
+
+        number_of_termo = int(input())
+
+        if number_of_termo == 1:
+
+            a = 3.9083 * 10e-4
+            b = -5.775 * 10e-9
+            c = -4.183 * 10e-16
+
+            print("Введите нулевое сопротивление: ", end='')
+            zero_resist = float(input())
+
+            print("Введите температуру: ", end='')
+            temperature = float(input())
+
+            if -200 <= temperature < 0:
+
+                result = zero_resist * (
+                        1 + a * temperature + b * math.pow(temperature, 2) + c * (temperature - 100) * math.pow(
+                    temperature, 3))
+
+                print(result)
+
+            elif 0 < temperature <= 850:
+
+                result = zero_resist * (1 + a * temperature + b * math.pow(temperature, 2))
+
+                print(result)
+
+        elif number_of_termo == 2:
+
+            a = 3.9690*10e-4
+            b = -5.841 * 10e-9
+            c = -4.330 * 10e-16
+
+            print("Введите нулевое сопротивление: ", end='')
+            zero_resist = float(input())
+
+            print("Введите температуру: ", end='')
+            temperature = float(input())
+
+            if -200 <= temperature < 0:
+
+                result = zero_resist * (
+                        1 + a * temperature + b * math.pow(temperature, 2) + c * (temperature - 100) * math.pow(
+                    temperature, 3))
+
+                print(result)
+
+            elif 0 < temperature <= 850:
+
+                result = zero_resist * (1 + a * temperature + b * math.pow(temperature, 2))
+
+                print(result)
+        elif number_of_termo == 3:
+
+            a = 4.28 * 10e-4
+            b = -6.2032 * 10e-9
+            c = 8.5154 * 10e-13
+
+            print("Введите нулевое сопротивление: ", end='')
+            zero_resist = float(input())
+
+            print("Введите температуру: ", end='')
+            temperature = float(input())
+
+            if -180 <= temperature < 0:
+
+                result = zero_resist * (
+                        1 + a * temperature + b * temperature*(temperature+6.7) + c * math.pow(
+                    temperature, 3))
+
+                print(result)
+
+            elif 0 < temperature <= 200:
+
+                result = zero_resist * (1 + a*temperature)
+
+                print(result)
+        elif number_of_termo == 4:
+
+            a = 5.4963 * 10e-4
+            b = 6.7556 * 10e-7
+            c = 9.2004 * 10e-12
+
+            print("Введите нулевое сопротивление: ", end='')
+            zero_resist = float(input())
+
+            print("Введите температуру: ", end='')
+            temperature = float(input())
+
+            if -60 <= temperature < 100:
+
+                result = zero_resist * (1 + a * temperature + b * math.pow(temperature,2))
+
+                print(result)
+
+            elif 100 < temperature <= 180:
+
+                result = zero_resist * (1 + a*temperature+b*math.pow(temperature,2)+c*math.pow(temperature,2)*(temperature-100))
+
+                print(result)
---
+++ main.py
@@ -0,0 +1,3 @@
+import Choose_type
+
+print(Choose_type.make_choice())
